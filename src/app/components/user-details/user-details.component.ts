a41298bc3c71ae3d78a62e1fa9ac28af7d8b68e6
Martozhyna <mybonanzaluck@gmail.com>
2023-05-25 02:32:25 +0300

    add lesson 1

---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {IUser} from "../../interfaces";
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent {
+
+  @Input()
+  user:IUser
+
+  userId:number
+
+  getPosts():void {
+    this.userId = this.user.id
+
+  }
+}
